009452474522f7925b4c18fe772ea1995c670fa2
Maayan Shalom <sha.maayan@gmail.com>
2020-08-19 01:20:09 +0300

    df['fold'].unique()

--- encoding.py
+++ encoding.py
@@ -11,15 +11,17 @@ df = pd.read_csv(metadata_file)
 
 files = []
 srs = []
-for idx, row in df.iterrows():
-    file_name = os.path.join(os.path.abspath(root_dir), 'fold' + str(row['fold']) + '/', row['slice_file_name'])
-    audio, sr = sf.read(file_name)
-    files.append(audio)
-    srs.append(sr)
+for f in df['fold'].unique():
+    print(f'Fold: {f}')
+    for idx, row in df[df['fold'] == f].iterrows():
+        file_name = os.path.join(os.path.abspath(root_dir), 'fold' + str(row['fold']) + '/', row['slice_file_name'])
+        audio, sr = sf.read(file_name)
+        files.append(audio)
+        srs.append(sr)
 
-emb_list, ts_list = openl3.get_audio_embedding(files, srs, content_type="env", batch_size=256)
-encode_dataset = torch.tensor([])
-for e in emb_list:
-    encoding = torch.tensor(e.mean(axis=0))
-    encode_dataset = torch.cat((encode_dataset, encoding))
-torch.save(encode_dataset, 'emb.pt')
+    emb_list, ts_list = openl3.get_audio_embedding(files, srs, content_type="env", batch_size=256)
+    encode_dataset = torch.tensor([])
+    for e in emb_list:
+        encoding = torch.tensor(e.mean(axis=0))
+        encode_dataset = torch.cat((encode_dataset, encoding))
+    torch.save(encode_dataset, f'emb_{f}.pt')
