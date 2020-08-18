41f4ad5a0c3f10824cd5f980ad1bd7ac389ad9e7
Maayan Shalom <sha.maayan@gmail.com>
2020-08-19 00:32:08 +0300

    get_audio_embedding(files)

--- encoding.py
+++ encoding.py
@@ -7,14 +7,12 @@ root_dir = './UrbanSound8K/audio/'
 metadata_file = './UrbanSound8K/metadata/UrbanSound8K.csv'
 df = pd.read_csv(metadata_file)
 
+files = []
+srs = []
 for idx, row in df.iterrows():
     file_name = os.path.join(os.path.abspath(root_dir), 'fold' + str(row['fold']) + '/', row['slice_file_name'])
     audio, sr = sf.read(file_name)
-    emb, ts = openl3.get_audio_embedding(audio, sr)
-    row['sr'] = sr
-    row['ts'] = ts
-    row['raw_len'] = audio.shape[0]
-    row['ch'] = audio.shape[1]
-    row['emb_len'] = emb.shape[1]
+    files.append(audio)
+    srs.append(sr)
 
-df.to_csv('temp.csv')
+emb_list, ts_list = openl3.get_audio_embedding(files, srs, batch_size=256)
