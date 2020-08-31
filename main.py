b07ac24613e7b3059a202dc16db4fa742b96e5a5
Maayan Shalom <sha.maayan@gmail.com>
2020-08-31 21:46:05 +0300

    clean main

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import torch
 from torch.optim import Adam
-from torchvision.transforms import Compose
 from torchsummary import summary
 
 import loader
 from trainer import Trainer, TrainConfig
 import network
-import transforms
 
 
 if __name__ == '__main__':
@@ -14,11 +12,8 @@ if __name__ == '__main__':
     batch_size = 2048
     learning_rate = 1e-3
     in_channels = 1
-    num_hidden = 128
     num_residual_hidden = 32
     num_residual_layers = 2
-
-    max_frames = 176400
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
@@ -28,10 +23,6 @@ if __name__ == '__main__':
     optimizer = Adam(params=model.parameters(), lr=learning_rate)
     criterion = torch.nn.CrossEntropyLoss(reduction='mean')
 
-    compose = Compose([
-        transforms.CutAndPaste(max_frames),
-        transforms.Spectrogram()
-    ])
     loaders = loader.UrbanMelSpectrogramLoader().get(batch_size)
 
     config = TrainConfig(model, loaders, criterion, optimizer)
