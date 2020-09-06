b9aeda15de64e3dfc6b135c7c29b35bf497f4dea
Maayan Shalom <sha.maayan@gmail.com>
2020-09-06 22:49:18 +0300

    loader str

--- loader.py
+++ loader.py
@@ -14,6 +14,9 @@ class Loader:
     def __len__(self):
         return len(self.data['train']) + len(self.data['val'])
 
+    def __str__(self):
+        return str(self.data['train'])
+
     def get(self, batch_size):
         loaders = dict()
         loaders['train'] = DataLoader(self.data['train'], batch_size=batch_size, shuffle=True, pin_memory=True)
@@ -24,20 +27,17 @@ class Loader:
 class UrbanSound8KLoader(Loader):
     def __init__(self):
         super(UrbanSound8KLoader, self).__init__()
-        root_dir = './data/UrbanSound8K/audio/'
-        metadata_file = './data/UrbanSound8K/metadata/UrbanSound8K.csv'
-        self.data['train'] = dataset.UrbanSound8K(metadata_file, root_dir, train=True)
-        self.data['val'] = dataset.UrbanSound8K(metadata_file, root_dir, train=False)
+        self.data['train'] = dataset.UrbanReduced(train=True)
+        self.data['val'] = dataset.UrbanReduced(train=False)
 
 
 class UrbanMelSpectrogramLoader(Loader):
     def __init__(self):
         super(UrbanMelSpectrogramLoader, self).__init__()
-        root_dir = './data/UrbanSound8K/'
         size = 8732
         val_size = 800
         data_indices = np.arange(0, size)
         val_indices = np.random.choice(data_indices, val_size, replace=False)
         train_indices = np.array(list(set(data_indices) - set(val_indices)))
-        self.data['train'] = dataset.UrbanMelSpectrogram(root_dir, train_indices)
-        self.data['val'] = dataset.UrbanMelSpectrogram(root_dir, val_indices)
+        self.data['train'] = dataset.UrbanMelSpectrogram(train_indices)
+        self.data['val'] = dataset.UrbanMelSpectrogram(val_indices)
