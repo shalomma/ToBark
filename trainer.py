d53443e4a92b6c9cc2a5ca09576bc5498c3164a8
Maayan Shalom <sha.maayan@gmail.com>
2020-09-07 02:23:48 +0300

    traincache load with timestamp

--- trainer.py
+++ trainer.py
@@ -59,8 +59,8 @@ class TrainCache:
             torch.save(model.state_dict(), f)
 
     @classmethod
-    def load(cls, model_class, directory):
-        with open(f'{directory}/params.pkl', 'rb') as f:
+    def load(cls, model_class, timestamp):
+        with open(f'models/{timestamp}/params.pkl', 'rb') as f:
             params = pickle.load(f)
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         args = list(inspect.signature(model_class.__init__).parameters.keys())
@@ -68,7 +68,7 @@ class TrainCache:
         for a in args[1:]:
             params_class[a] = params[a]
         model = model_class(**params_class).to(device)
-        with open(f'{directory}/model.pt', 'rb') as f:
+        with open(f'models/{timestamp}/model.pt', 'rb') as f:
             state_dict = torch.load(f, map_location=device)
             model.load_state_dict(state_dict)
         model.eval()
