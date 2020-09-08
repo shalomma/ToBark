892e32c7cd84e34260e9066760c4cf9c4915b9e4
Maayan Shalom <sha.maayan@gmail.com>
2020-09-09 02:12:36 +0300

    drop loader's dicts

--- loader.py
+++ loader.py
@@ -12,32 +12,32 @@ class Loader:
         assert 0 < train_ratio < 1, '0 < r < 1'
         np.random.seed(self.seed)
         manual_seed(self.seed)
+        self.dataset = dataset
         train_size = int(dataset.size * train_ratio)
-        data_indices = np.arange(0, dataset.size)
-        indices = np.random.choice(data_indices, train_size, replace=False)
+
+        self.data_indices = np.arange(0, dataset.size)
+        indices = np.random.choice(self.data_indices, train_size, replace=False)
         self.indices = {
             'train': indices,
-            'val': np.array(list(set(data_indices) - set(indices)))
-        }
-        self.data = {
-            'train': copy.copy(dataset(self.indices['train'])),
-            'val': copy.copy(dataset(self.indices['val']))
+            'val': np.array(list(set(self.data_indices) - set(indices)))
         }
 
     def __len__(self):
-        return len(self.data['train']) + len(self.data['val'])
+        return len(self.data_indices)
 
     def __str__(self):
-        return str(self.data['train'])
+        return str(self.dataset)
 
     def get(self, batch_size, pin_memory=False):
-        loaders = dict()
-        loaders['train'] = DataLoader(self.data['train'], batch_size=batch_size, shuffle=True, pin_memory=pin_memory)
-        loaders['val'] = DataLoader(self.data['val'], batch_size=batch_size, shuffle=True, pin_memory=pin_memory)
-        print(f"{self.data['train']} train (size={len(self.data['train'])})")
-        print(f"{self.data['val']} val (size={len(self.data['val'])})")
-        return loaders
+        print(f"{self.__str__} train (size={len(self.indices['train'])})")
+        print(f"{self.__str__} val (size={len(self.indices['val'])})")
+        return {
+            'train': DataLoader(copy.copy(self.dataset(self.indices['train'])),
+                                batch_size=batch_size, shuffle=True, pin_memory=pin_memory),
+            'val': DataLoader(copy.copy(self.dataset(self.indices['val'])),
+                              batch_size=batch_size, shuffle=True, pin_memory=pin_memory)
+        }
 
     def get_all(self, batch_size, pin_memory=False):
-        return DataLoader(self.data['train'] + self.data['val'],
+        return DataLoader(copy.copy(self.dataset(self.data_indices)),
                           batch_size=batch_size, shuffle=True, pin_memory=pin_memory)
