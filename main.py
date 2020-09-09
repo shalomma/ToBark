1d2377d90556edf8d42affbe2c381fababcf35ba
Maayan Shalom <sha.maayan@gmail.com>
2020-09-09 17:10:23 +0300

    main args

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import torch
+import argparse
 import torch.optim as optim
 from torchsummary import summary
 
@@ -9,12 +10,19 @@ import network
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', type=int, help='maximal number of evaluation', default=150)
+    parser.add_argument('--binary', dest='binary', help='binary classification', action='store_true')
+    args = parser.parse_args()
+
     params = {
-        'epochs': 150,
+        'epochs': args.epochs,
         'batch_size': 256,
-        'learning_rate': 1e-3,
+        'learning_rate': 5e-4,
+        'weight_decay': 0.1,
         'in_channels': 1,
-        'n_classes': 2,
+        'pos_weight': 2.,
+        'n_classes': 2 if args.binary else 10
     }
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -22,13 +30,19 @@ if __name__ == '__main__':
 
     model = network.MelCNN2d(in_channels=params['in_channels'], n_classes=params['n_classes']).to(device)
     summary(model, input_size=(params['in_channels'], 16, 8))
-    optimizer = optim.Adam(params=model.parameters(), lr=params['learning_rate'], weight_decay=0.1)
+    optimizer = optim.Adam(params=model.parameters(), lr=params['learning_rate'], weight_decay=params['weight_decay'])
     scheduler = optim.lr_scheduler.CyclicLR(optimizer, params['learning_rate'],
                                             10 * params['learning_rate'], cycle_momentum=False)
-    criterion = torch.nn.CrossEntropyLoss(weight=torch.tensor([1., 2.]).to(device), reduction='sum')
+    criterion = torch.nn.CrossEntropyLoss(weight=torch.tensor([1., params['pos_weight']]).to(device), reduction='sum')
 
-    prefixes = ['UrbanSound8K_binary', 'CatsAndDogs_binary']
-    dataset = MelSpecEncoded(prefixes)
+    prefixes = ['UrbanSound8K', 'CatsAndDogs']
+    if args.binary:
+        prefixes = [p + '_binary' for p in prefixes]
+        dataset = MelSpecEncoded(prefixes)
+        dataset.binarize()
+    else:
+        dataset = MelSpecEncoded(prefixes)
+
     loaders = ld.Loader(params['batch_size']).get(dataset)
 
     config = TrainConfig(model, loaders, criterion, optimizer, scheduler)
